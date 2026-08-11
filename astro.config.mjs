a361d2239b271f2b35e19a8f897fc7039cd5d073
Markus Gerstenberg <38734019+degersti@users.noreply.github.com>
2026-08-12 01:49:48 +0200

    Update site URL in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,14 @@
 // @ts-check
-
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import { defineConfig, fontProviders } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
+	site: 'https://degersti.github.io',
+
 	integrations: [mdx(), sitemap()],
+
 	fonts: [
 		{
 			provider: fontProviders.local(),
